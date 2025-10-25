72a526e3e64f8c76548d0836ad88692c1bdf19c7
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2025-10-25 15:38:14 +0800

    a

--- compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set PATH=C:\msys64\ucrt64\bin;%PATH%
-gcc.exe %1 -o %~n1.exe
-if errorlevel 1 (
-    echo Compilation failed!
-) else (
-    echo Compilation successful! Run %~n1.exe to execute.
-)
