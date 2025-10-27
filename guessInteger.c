13ce0481175c2409442cdfe9508a1c37f4fe59cb
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2025-10-27 23:21:53 +0800

    Update guessInteger.c

--- guessInteger.c
+++ guessInteger.c
@@ -65,13 +65,13 @@ int main() {
     }
 
     printf("Player 2, you have 10 guesses remaining.\n");
-    printf("Enter your guess:");
+    printf("Enter your guess:\n");
     int guess;
     while (guesses > 0) {
         scanf("%d", &guess);
         if (guess < 1 || guess > 1000) { // Validate Player 2's input
             printf("That number is out of range.\n");
-            printf("Enter your guess:");
+            printf("Enter your guess:\n");
             continue;
         }
         if (guess == number1) {
@@ -89,7 +89,7 @@ int main() {
             } else {
                 printf("Player 2, you have %d guesses remaining.\n", guesses);
             }
-            printf("Enter your guess:");
+            printf("Enter your guess:\n");
         }
     }
     printf("Player 1 wins.\n");
